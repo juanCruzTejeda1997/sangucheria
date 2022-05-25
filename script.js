6c096e3515dee5d678b8377d0deb22063ba469e0
juanCruzTejeda1997 <you@example.com>
2022-05-25 18:07:04 -0300

    arreglos style y js

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ var base = document.getElementById("base");
 var pan = document.getElementById("pan");
 var cYa= document.getElementById("c-a");
 var fac = document.getElementById("total");
-
+var carne=200;
+var pollo=150;
+var veg=100;
+var panN=60;
+var panB=50;
+var panSG=75;
 var total=0;
  function detBase(base){
     if( base.value.toLowerCase() == "carne"){
-        total=total+200;
+        total=total+carne;
     }else{
 
         if(base.value.toLowerCase() == "pollo"){
-            total = total +150;
+            total = total +pollo;
         }else{
 
             if(base.value.toLowerCase()=="vegetariano" || base.value.toLowerCase()=="vegetariana"){
-                total = total + 100;
+                total = total + veg;
         }else{
             alert("La opcion de Base Ingresada es Incorrecta");
     }
@@ -25,37 +30,44 @@ var total=0;
 
 function detPan(pan){
     if(pan.value.toLowerCase()=="negro"){
-        total=total+60;
+        total=total+panN;
     }else{
         if(pan.value.toLowerCase()=="blanco"){
-            total=total+50;
+            total=total+panB;
         }else{
             if(pan.value.toLowerCase()=="sin gluten"){
-                total=total+75;
+                total=total+panSG;
             }
         }
     }
 }
+var queso=20;
+var tomate=15;
+var cebolla=15;
+var lechuga=10;
+var mos=5;
+var mayo=5;
+
 function detCyA(cYa){
     var vectorCyA= cYa.value.split(",");/*queso tomate lechuga cebolla mayo mostaza */
     for(i=0; i < vectorCyA.length;i++){
         if(vectorCyA[i].toLowerCase()=="queso"){
-            total=total+20;
+            total=total+queso;
         }else{
             if(vectorCyA[i].toLowerCase()=="tomate"){
-                total=total+15;
+                total=total+tomate;
             }else{
                 if(vectorCyA[i].toLowerCase()=="cebolla"){
-                    total=total+15;
+                    total=total+cebolla;
                 }else{
                     if(vectorCyA[i].toLowerCase()=="lechuga"){
-                        total=total+10;
+                        total=total+lechuga;
                     }else{
                         if(vectorCyA[i].toLowerCase()=="mostaza"){
-                            total=total+5;
+                            total=total+mos;
                         }else{
                             if(vectorCyA[i].toLowerCase()=="mayonesa"){
-                                total=total+5;
+                                total=total+mayo;
                             }
                         }
                     }
@@ -63,7 +75,7 @@ function detCyA(cYa){
             }
         }
     }
-    console.log(vectorCyA[0]);
+    
 
 }
 
